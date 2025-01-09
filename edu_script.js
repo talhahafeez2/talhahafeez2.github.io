5a4d9db89a8d341f05a97387c87831584eb8bab1
talhahafeez2 <talhahafeez03@gmail.com>
2025-01-08 22:26:43 -0500

    Updated Work Experience and Education and Certification pages

--- edu_script.js
+++ edu_script.js
@@ -1,25 +1,57 @@
 window.onload = function() {
-    var edu = [];
+    var edu = {};
 
     $.getJSON('./info_files/edu.json', function (data) {
         for (const key in data) {
-            edu.push([key, data[key]]);
+            edu[key] = data[key];
         }
+        console.log(edu);
         add_edu(edu);
     });
 
     function add_edu(edu) {
         var d = document.getElementById('edu');
-        edu.forEach(element => {
-            var ul = document.createElement('ul');
-            d.innerHTML += "<h3>My " + element[0] + " Include:</h3>"
-            element[1].forEach(element2 => {
-                var li = document.createElement("li");
-                li.innerText = element2;
+        var d2 = document.getElementById('certs');
+        var ul = document.createElement('ul');
+        var ul2 = document.createElement('ul');
+        var count = 0;
+        for (const element in edu) {
+            var li = document.createElement("li");
+            li.innerText = element;
+            li.classList.add("main_li");
+            if (count == 0) {
                 ul.appendChild(li);
-            });
-            d.appendChild(ul);
-        });
+            } else {
+                ul2.appendChild(li);
+            }
+            console.log(edu[element]);
+            for (const val in edu[element]) {
+                var li2 = document.createElement("li");
+                
+                li2.innerText = edu[element][val];
+                li2.classList.add("inner_li1");
+                if (count == 0) {
+                    ul.appendChild(li2);
+                } else {
+                    ul2.appendChild(li2);
+                }
+            }
+
+            count += 1;
+
+        }
+        d.appendChild(ul);
+        d2.appendChild(ul2);
+        // edu.forEach(element => {
+        //     var ul = document.createElement('ul');
+        //     d.innerHTML += "<h3>My " + element[0] + " Include:</h3>"
+        //     element[1].forEach(element2 => {
+        //         var li = document.createElement("li");
+        //         li.innerText = element2;
+        //         ul.appendChild(li);
+        //     });
+        //     d.appendChild(ul);
+        // });
     }
     
 }
--- qual_script.js
+++ qual_script.js
@@ -1,47 +1,34 @@
 window.onload = function() {
-    var bio = [];
+    var bio = {};
 
     $.getJSON('./info_files/bio.json', function (data) {
-        var tech_skills = [];
         for (const key in data) {
-            if (key.includes("Technical Skills: ")) {
-                for (const i in data[key]) {
-                    tech_skills.push([i, data[key][i]]);
-                }
-            }
-            bio.push(key);
+            bio[key] = data[key];
         }
-        add_skills(bio, tech_skills);
+        add_skills(bio);
     });
 
-    function add_skills(bio, tech_skills) {
+    function add_skills(bio) {
         var d = document.getElementById('bio');
+        var d2 = document.getElementById('skills');
         var ul = document.createElement("ul");
         var ul2 = document.createElement("ul");
-        bio.forEach(element => {
+        for (const element in bio) {
             var l = document.createElement("li");
-            ul.appendChild(l);
             l.innerText = element;
             l.classList.add("main_li");
-            if (element.includes("Technical Skills: ")) {
-                tech_skills.forEach(element2 => {
-                    var ul3 = document.createElement("ul");
-                    var l2 = document.createElement("li");
-                    l2.innerText = element2[0];
-                    l2.classList.add("inner_li1");
-                    ul2.appendChild(l2);
-                    if (element2[1] != "") {
-                        ul2.appendChild(ul3);
-                        var l3 = document.createElement("li");
-                        l3.innerText = element2[1];
-                        l3.classList.add("inner_li2");
-                        ul3.appendChild(l3);
-                    }
-                    ul.appendChild(ul2);
-                });
+            ul2.appendChild(l);
+            if (bio[element] != "") {
+                var l2 = document.createElement("li");
+                l2.innerText = bio[element];
+                l2.classList.add("inner_li1");
+                ul2.appendChild(l2);
+            } else {
+                ul.appendChild(l);
             }
-        });
+        }
         d.appendChild(ul);
+        d2.appendChild(ul2);
     }
     
 }
